af92446b18372d81ea086305a175abf3a441d651
Mounika-Thirupathi <thirupathim848@gmail.com>
2025-08-25 19:14:21 +0530

    initial msg

---
+++ counter(Js)/script.js
@@ -0,0 +1,37 @@
+
+function Increment(){
+   let currentValue= document.getElementById("counter").textContent
+   currentValue=parseInt(currentValue)
+   let count=document.getElementById("counter").textContent=currentValue+1
+   count=parseInt(count)
+   if(count>0){
+    document.getElementById("counter").style.color="blue"
+   }
+   else if(count<0){
+    document.getElementById("counter").style.color="red"
+   }else{
+    document.getElementById("counter").style.color="black"
+   }
+}
+
+function Decrement(){
+    let currentValue=document.getElementById("counter").textContent
+    currentValue=parseInt(currentValue)
+    let count =document.getElementById("counter").textContent=currentValue-1
+    count=parseInt(count)
+   if(count>0){
+    document.getElementById("counter").style.color="blue"
+   }
+   else if(count<0){
+    document.getElementById("counter").style.color="red"
+   }else{
+    document.getElementById("counter").style.color="black"
+   }
+
+
+}
+
+function Reset(){
+    document.getElementById("counter").textContent=0
+    document.getElementById("counter").style.color="black"
+}
